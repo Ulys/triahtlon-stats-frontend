41f9dce3672a24b3b50a3f043919af96830866c8
Anton Razumovskyy <anton.razumovskyy@gmail.com>
2017-01-21 18:21:11 +0200

    initial commit

---
+++ src/components/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello-world">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        msg: 'Hello world'
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
