8087c8ca95ed2e271649d1ffb41cab2bf224b3f8
Anton Razumovskyy <anton.razumovskyy@gmail.com>
2017-01-26 21:09:46 +0200

    Added Header component

---
+++ src/components/home/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="mdl-layout__header">
+    <div class="mdl-layout-icon"></div>
+    <div class="mdl-layout__header-row">
+      <div class="mdl-layout__title">
+        Triathlon Stats
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'header-component'
+  }
+</script>
+<style scoped></style>
